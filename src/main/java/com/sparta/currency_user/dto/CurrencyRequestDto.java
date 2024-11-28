a2d04c6b1a55ee20e320401bd52ac4cf7423e1cd
Seonu-Jeong <sw.jeong97@gmail.com>
2024-11-29 00:45:34 +0900

    Feat : CurrencyController Validation 적용

--- src/main/java/com/sparta/currency_user/dto/CurrencyRequestDto.java
+++ src/main/java/com/sparta/currency_user/dto/CurrencyRequestDto.java
@@ -1,14 +1,22 @@
 package com.sparta.currency_user.dto;
 
 import com.sparta.currency_user.entity.Currency;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class CurrencyRequestDto {
+
+    @NotBlank
     private String currencyName;
+
+    @NotNull
     private BigDecimal exchangeRate;
+
+    @NotBlank
     private String symbol;
 
     public Currency toEntity() {
