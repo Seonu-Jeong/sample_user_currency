fefe4baf4c20a40c7c33117c89fb2c3ff3665e9f
Seonu-Jeong <sw.jeong97@gmail.com>
2024-11-29 02:26:31 +0900

    Feat : findUsersTotalExchangeInfo() JPQL 추가

--- src/main/java/com/sparta/currency_user/repository/ExchangeRepository.java
+++ src/main/java/com/sparta/currency_user/repository/ExchangeRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.currency_user.repository;
 
+import com.sparta.currency_user.dto.ExchangeTotalInfoDto;
+import com.sparta.currency_user.entity.User;
 import com.sparta.currency_user.entity.UserCurrency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +15,13 @@ import java.util.Optional;
 public interface ExchangeRepository extends JpaRepository<UserCurrency,Long> {
 
     List<UserCurrency> findByUserId(Long userId);
+
+
+    @Query("select new com.sparta.currency_user.dto.ExchangeTotalInfoDto(count(uc), sum(uc.amountInKrw)) " +
+            "from UserCurrency uc where uc.user = :user and uc.status = 'NORMAL' group by uc.user")
+    Optional<ExchangeTotalInfoDto> findUsersTotalExchangeInfo(
+            @Param("user") User user
+    );
+
+
 }
