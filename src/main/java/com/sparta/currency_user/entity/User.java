7935ed6c6d6c834fae823210dedc6116f3a0c601
Seonu-Jeong <sw.jeong97@gmail.com>
2024-11-29 09:29:15 +0900

    Feat : User 클래스에 JPA Auditing 적용

--- src/main/java/com/sparta/currency_user/entity/User.java
+++ src/main/java/com/sparta/currency_user/entity/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Entity
 @Getter
-public class User {
+public class User extends TimeBaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
